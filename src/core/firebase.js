c26015eff61dce22dbdd880bf49bd16b8eeb2f56
jaguark11 <kristiandavid2011@gmail.com>
2026-03-29 15:42:29 -0500

    reto 7: crud completo y firebase auth

---
+++ src/core/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC7cCHbOHiDmlI-WEF8tff43W5mMuNVvIk",
+  authDomain: "estructura-a7c65.firebaseapp.com",
+  projectId: "estructura-a7c65",
+  storageBucket: "estructura-a7c65.firebasestorage.app",
+  messagingSenderId: "630561703103",
+  appId: "1:630561703103:web:a2041fa3765a47c558720a"
+};
+
+const app = initializeApp(firebaseConfig);
+export const authInstance = getAuth(app);
